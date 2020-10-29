a8ff8103f1ddb7064ec637c63e40ff5308e44aac
Suaysh Chandra <suyash.chandra24@gmail.com>
2020-10-29 11:41:59 +0530

    Better Images

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -4,11 +4,13 @@ import "./Pokecard.css";
 class Pokecard extends Component{
     render() {
         const arr =this.props
+        const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+        const pokeId= (number) => (number <=999 ? `00${number}`.slice(-3): number)
         console.log(arr)
         return(
             <div className="Pokecard">
             <h3 className="Pokecard-title">{arr.name}</h3>
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${arr.id}.png`} />
+            <img src={`${POKE_API}${pokeId(arr.id)}.png`} />
         <div className="Pokecard-data">Type: {arr.type}</div>
         <div className="Pokecard-data">EXP: {arr.exp}</div>
             </div>
